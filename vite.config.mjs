2f893216e7fa5234e7dee068dfd286730c9eb353
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2026-02-04 11:24:47 -0500

    Fix: mroe tailwind stuff
    Signed-off-by: Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isUserOrOrgPage = repoName?.endsWith('.github.io');
+const base = process.env.GITHUB_PAGES === 'true' && repoName && !isUserOrOrgPage
+  ? `/${repoName}/`
+  : '/';
+
 export default defineConfig({
+  base,
   plugins: [
     viteStaticCopy({
       targets: [
